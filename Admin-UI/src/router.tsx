6deeba3a89d9262280dc4c0eb0258c2ea3659d06
Riya Tyagi <130086862+Riyatyagi284@users.noreply.github.com>
2026-04-28 16:28:07 +0530

    Admin-ui code

---
+++ Admin-UI/src/router.tsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/login/login';
+import Dashboard from './layouts/Dashboard';
+import NonAuth from './layouts/NonAuth';
+import Root from './layouts/Root';
+import Users from './pages/users/Users';
+import Tenants from './pages/tenants/Tenants';
+import Products from './pages/products/Products';
+import Orders from './pages/orders/Orders';
+import SingleOrder from './pages/orders/SingleOrder';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+            {
+                path: '',
+                element: <Dashboard />,
+                children: [
+                    {
+                        path: '',
+                        element: <HomePage />,
+                    },
+                    {
+                        path: '/users',
+                        element: <Users />,
+                    },
+                    {
+                        path: '/restaurants',
+                        element: <Tenants />,
+                    },
+                    {
+                        path: '/products',
+                        element: <Products />,
+                    },
+                    {
+                        path: '/orders',
+                        element: <Orders />,
+                    },
+                    {
+                        path: '/orders/:orderId',
+                        element: <SingleOrder />,
+                    },
+                ],
+            },
+            {
+                path: '/auth',
+                element: <NonAuth />,
+                children: [
+                    {
+                        path: 'login',
+                        element: <LoginPage />,
+                    },
+                ],
+            },
+        ],
+    },
+]);
